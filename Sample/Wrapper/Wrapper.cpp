800ebe43e5c447837a897d1450c45760526dfa7a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-08-08 10:29:44 +0900

    ラッパークラスを実装

--- Sample/Wrapper/Wrapper.cpp
+++ Sample/Wrapper/Wrapper.cpp
@@ -8,6 +8,17 @@ namespace Wrapper {
 
     Boolean MyTreeView::drawToTreeView(System::Windows::Forms::TreeView^ target)
     {
+        target->Nodes->Clear();
+
+        System::Windows::Forms::TreeNode^   node1 = gcnew System::Windows::Forms::TreeNode("Node 1");
+        target->Nodes->Add(node1);
+
+        System::Windows::Forms::TreeNode^   node2 = gcnew System::Windows::Forms::TreeNode("Node 1-1");
+        node1->Nodes->Add(node2);
+
+        System::Windows::Forms::TreeNode^   node3 = gcnew System::Windows::Forms::TreeNode("Node 1-2");
+        node1->Nodes->Add(node3);
+
         return true;
     }
 }
--- Sample/Wrapper/Wrapper.h
+++ Sample/Wrapper/Wrapper.h
@@ -8,7 +8,6 @@ namespace Wrapper {
 
 	public ref class MyTreeView
 	{
-		// TODO: Add your methods for this class here.
     public:
         Boolean drawToTreeView(System::Windows::Forms::TreeView^ target);
 	};
