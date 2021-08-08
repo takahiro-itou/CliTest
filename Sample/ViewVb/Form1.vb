b3946140f865326d2c77f09d03475e82dbfb72f4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-08-08 10:07:14 +0900

    GUI からラッパー部分の呼び出し

--- Sample/ViewVb/Form1.vb
+++ Sample/ViewVb/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        Dim obj As Wrapper.MyTreeView
+        obj = New Wrapper.MyTreeView()
+        obj.drawToTreeView(Me.TreeView1)
     End Sub
 End Class
